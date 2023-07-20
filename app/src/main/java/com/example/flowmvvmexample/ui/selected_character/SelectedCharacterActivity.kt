857512f436c5d9430d97ddb75f304b693a4719ab
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-07-20 12:00:08 +0300

    add design for selectedCharacter Activity

--- app/src/main/java/com/example/flowmvvmexample/ui/selected_character/SelectedCharacterActivity.kt
+++ app/src/main/java/com/example/flowmvvmexample/ui/selected_character/SelectedCharacterActivity.kt
@@ -1,22 +1,27 @@
 package com.example.flowmvvmexample.ui.selected_character
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.example.flowmvvmexample.R
 import com.example.flowmvvmexample.common.utils.OnCharacterClick
+import com.example.flowmvvmexample.common.utils.Resource
+import com.example.flowmvvmexample.databinding.ActivitySelectedCharacterBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SelectedCharacterActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySelectedCharacterBinding
     val viewModel: SelectedCharacterViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selected_character)
 
         initUi()
         subscribeToCharacterResponse()
@@ -24,6 +29,10 @@ class SelectedCharacterActivity : AppCompatActivity() {
 
     }
     private fun initUi() {
+        binding= ActivitySelectedCharacterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         var id = intent.extras!!.getInt("characterId")
         lifecycleScope.launch {
             viewModel.getCharacterData(id)
@@ -33,6 +42,27 @@ class SelectedCharacterActivity : AppCompatActivity() {
         lifecycleScope.launch {
             viewModel.liveCharacterData.collect { it ->
                 Log.e("testResponse", it.data.toString())
+                when(it){
+                    is Resource.Loading -> {
+                        binding.progressPar.visibility=View.VISIBLE
+                    }
+                    is Resource.DataError -> {
+                        binding.progressPar.visibility=View.GONE
+
+                    }
+                    is Resource.Success -> {
+                        binding.progressPar.visibility=View.GONE
+                        binding.apply {
+                            characterName.text=it.data!!.name
+                            characterImage.load(it.data.image) {
+                                crossfade(true)
+                                crossfade(1000)
+                            }
+
+                        }
+
+                    }
+                }
             }
         }
     }
